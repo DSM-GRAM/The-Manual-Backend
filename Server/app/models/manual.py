0974ab15ecb14894228a3fae1beae85bdd03a800
miraedbswo <miraedbswo@gmail.com>
2018-08-09 23:33:03 +0900

    incomplete

--- Server/app/models/manual.py
+++ Server/app/models/manual.py
@@ -4,20 +4,34 @@ from app.models import BaseModel
 
 
 class Manual(BaseModel):
-    pass
+
+    title = CharField(
+        null=False
+    )
     # 메뉴얼의 제목
+
+    # how to use ListField in peewee?
     # 같이 하는 멤버
 
 
 class Main(BaseModel):
-    pass
-    # 프사 배사
+
+    # profile_image =
+
+    # background_image =
 
 
 class Album(BaseModel):
-    pass
+
+    title = CharField(
+        null=False
+    )
     # - 앨범의 제목
+
+    # image =
     # - 사진
+
+
     # - (어디서 했는지) 위치
 
     # - (매뉴얼 DB에 있는 사람 중)함께한 사람 추가
