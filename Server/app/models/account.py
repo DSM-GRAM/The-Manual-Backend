a07a58aef1343bd0f0d4efed39d5a51872ec11fc
miraedbswo <miraedbswo@gmail.com>
2018-08-09 23:31:07 +0900

    remove SignupModel

--- Server/app/models/account.py
+++ Server/app/models/account.py
@@ -3,9 +3,29 @@ from peewee import *
 from app.models import BaseModel
 
 
-class SignupModel(BaseModel):
-    pass
-
-
 class UserModel(BaseModel):
-    pass
+    id = CharField(
+        primary_key=True
+    )
+
+    pw_hashed = CharField(
+        null=False
+    )
+
+    email = CharField(
+        null=False
+    )
+
+    email_auth = BooleanField(
+        default=False
+    )
+
+    name = CharField(
+        null=False
+    )
+
+    friend = IntegerField(
+        default=0
+    )
+
+    # more
