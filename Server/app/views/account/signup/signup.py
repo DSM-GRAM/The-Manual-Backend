0f869eb4a129aa09eb2ae213e5df22d7cef2d92f
Gaegul <eunjuoi0515@gmail.com>
2018-08-11 15:37:06 +0900

    중복처리

--- Server/app/views/account/signup/signup.py
+++ Server/app/views/account/signup/signup.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, Response, abort, g, request
 from flask_restful import Api
 
-from app.models.account import UserModel
+from app.models.account import UserModel as User
 from app.views import api_blueprint, BaseResource
 
 api = Api(api_blueprint)
@@ -11,14 +11,18 @@ api.prefix = ''
 @api.resource('/check/id/<id>')
 class CheckIDDuplicated(BaseResource):
     def get(self, id):
-        """
-        ID 중복체크
-        """
+        query = User.select().where(User.id == id)
+        if query.exists():
+            return 406
+        else:
+            return 200
 
 
 @api.resource('/check/email/<email>')
 class CheckEmailDuplicated(BaseResource):
     def get(self, email):
-        """
-        이메일 중복체크
-        """
+        query = User.select().where(User.email == email)
+        if query.exists():
+            return 406
+        else:
+            return 200
